5bd2474d00fecffb4fb80e129e0b0f331a460829
zhou <212518970@qq.com>
2015-11-26 22:18:46 +0800

    B commit

---
+++ GitDemo/GitDemo/ViewControllerB.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerB.h
+//  GitDemo
+//
+//  Created by qianfeng007 on 15/11/26.
+//  Copyright © 2015年 Choul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerB : UIViewController
+
+@end
